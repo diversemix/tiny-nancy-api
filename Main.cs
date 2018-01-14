d17ad094e898d71bd431c6a38a340ab0ff866a34
Peter Hooper <p.hooper@iesohealth.com>
2018-01-14 17:53:58 +0000

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NancyApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000/")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tiny-nancy-api
 Smallest .NET Core API template
+
+Inspired by looking at...
+https://github.com/dotnet/dotnet-docker-samples
